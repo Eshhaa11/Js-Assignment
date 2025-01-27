6ca8754a83ef0e4b20063d22298aaea34e007701
Eshhaa11 <patellesha11@gmail.com>
2025-01-27 15:37:01 +0300

    q1 done

---
+++ q1.js
@@ -0,0 +1,16 @@
+const car = {
+    brand : 'Volvo',
+    model : 'EX90'
+
+};
+
+//There are two correct syntax to alert Volvo, name them
+
+//1) The bracket notation
+console.log(car['brand']);
+
+
+//2) THe dot notation
+console.log(car.brand);
+
+
